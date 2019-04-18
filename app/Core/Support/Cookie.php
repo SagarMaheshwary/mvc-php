01fd8e0ab9971f3434a5e7050069632379a8e8fb
Sagar Maheshwary <sagarmaheshwary31@gmail.com>
2019-04-18 17:56:35 +0500

    session and cookie class

--- app/Core/Support/Cookie.php
+++ app/Core/Support/Cookie.php
@@ -2,7 +2,54 @@
 
 namespace App\Core\Support;
 
+/**
+ * Handle all the stuff related to cookies.
+ */
 class Cookie
 {
+    
+    /**
+     * Get a value.
+     * 
+     * @param string $key
+     * @return mixed
+     */
+    public static function get($key)
+    {
+        return self::has($key) == true ? $_COOKIE[$key] : '';
+    }
+
+    /**
+     * Set a value.
+     * 
+     * @param string $key
+     * @param string $value
+     * @return bool
+     */
+    public static function set($key, $value, $expires = 0, $httpOnly = false, $path = '/',$domain = null,$secure = false)
+    {
+        setcookie($key,$value,$expires,$path,$domain,$secure,$httpOnly);
+    }
+
+    /**
+     * Determine if a value exists.
+     * 
+     * @param string $key
+     * @return bool
+     */
+    public static function has($key)
+    {
+        return isset($_COOKIE[$key]) ? true : false;
+    }
+
+    /**
+     * Unset/Remove a value.
+     * 
+     * @return void
+     */
+    public static function unset($key)
+    {
+        self::set($key,'');
+    }
 
 }
--- app/Core/Support/Session.php
+++ app/Core/Support/Session.php
@@ -1,8 +1,88 @@
 <?php
 
-namespace App\Support;
+namespace App\Core\Support;
 
+/**
+ * Handle all the stuff related to session.
+ */
 class Session
 {
     
+    /**
+     * Get a value.
+     * 
+     * @param string $key
+     * @return mixed
+     */
+    public static function get($key)
+    {
+        return self::has($key) == true ? $_SESSION[$key] : '';
+    }
+
+    /**
+     * Set a value.
+     * 
+     * @param string $key
+     * @param string $value
+     * @return bool
+     */
+    public static function set($key,$value)
+    {
+        return (bool)($_SESSION[$key] = $value);
+    }
+
+    /**
+     * Determine if a value exists.
+     * 
+     * @param string $key
+     * @return bool
+     */
+    public static function has($key)
+    {
+        return isset($_SESSION[$key]) ? true : false;
+    }
+
+    /**
+     * Unset/Remove a value.
+     * 
+     * @return void
+     */
+    public static function unset($key)
+    {
+        unset($_SESSION[$key]);
+    }
+
+    /**
+     * Completely destroy the session.
+     * 
+     * @return void
+     */
+    public static function destroy()
+    {
+        session_unset();
+        session_destroy();
+    }
+
+    /**
+     * Make the value available for the next request.
+     * (Flash message)
+     * 
+     * @param string $key
+     * @param string $value
+     * 
+     * @return mixed
+     */
+    public static function flash($key,$value)
+    {
+        if(self::has($key)){
+            //value exists so return and unset it.
+            $flash =self::get($key);
+            self::unset($key);
+            return $flash;
+        }else{
+            //value doesn't exists so set it.
+            self::set($key,$value);
+        }
+    }
+
 }
